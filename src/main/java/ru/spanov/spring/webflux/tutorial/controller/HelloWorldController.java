a9558680d2fb71ca6245048c8b71b7bcadcd0ba5
stanislav.panov <Stanislav.Panov@leroymerlin.ru>
2025-03-13 16:52:31 +0300

    virtual threads

--- src/main/java/ru/spanov/spring/webflux/tutorial/controller/HelloWorldController.java
+++ src/main/java/ru/spanov/spring/webflux/tutorial/controller/HelloWorldController.java
@@ -1,17 +1,24 @@
 package ru.spanov.spring.webflux.tutorial.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/tutorial")
 public class HelloWorldController {
 
-  @GetMapping
+  @GetMapping("/hello")
   public Mono<String> sayHello() {
     return Mono.just("Hello World!");
+  }
+
+  @GetMapping("/thread-info")
+  public Mono<String> threadInfo() {
+    return Mono.just(Thread.currentThread().toString());
   }
 
 }

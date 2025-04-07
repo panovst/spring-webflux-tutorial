1ab1a884dac4f37251b135df3ec13eecb1252aa7
stanislav.panov <Stanislav.Panov@leroymerlin.ru>
2025-04-07 14:41:39 +0300

    add WebClient call

--- src/main/java/ru/spanov/spring/webflux/tutorial/controller/HelloWorldController.java
+++ src/main/java/ru/spanov/spring/webflux/tutorial/controller/HelloWorldController.java
@@ -2,18 +2,40 @@ package ru.spanov.spring.webflux.tutorial.controller;
 
 import static java.time.Duration.ofMillis;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/tutorial")
 public class HelloWorldController {
 
-  @GetMapping
+  private final WebClient webClient = WebClient.create("http://localhost:8080");
+
+  @GetMapping("/hello-world")
   public Mono<String> sayHello() {
     return Mono.just("Hello World!").delayElement(ofMillis(500));
+  }
+
+  @GetMapping("/call-sum")
+  public Mono<String> callSum() {
+    return webClient
+        .get()
+        .uri("/calculation/sum?a=1&b=2")
+        .exchangeToMono(response -> {
+          if (response.statusCode().equals(HttpStatus.OK)) {
+            return response.bodyToMono(String.class);
+          } else if (response.statusCode().is4xxClientError()) {
+            return Mono.just("Error response");
+          } else {
+            return response.createException()
+                .flatMap(Mono::error);
+          }
+        });
   }
 
 }
---
+++ src/main/java/ru/spanov/spring/webflux/tutorial/controller/SumCalculationController.java
@@ -0,0 +1,20 @@
+package ru.spanov.spring.webflux.tutorial.controller;
+
+import static java.time.Duration.ofMillis;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/calculation")
+public class SumCalculationController {
+
+  @GetMapping("/sum")
+  public Mono<Integer> sum(@RequestParam int a, @RequestParam int b) {
+    return Mono.just(a + b).delayElement(ofMillis(500));
+  }
+
+}

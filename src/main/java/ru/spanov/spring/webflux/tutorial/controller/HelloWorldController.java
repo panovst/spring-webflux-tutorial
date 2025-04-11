518093c7c59a94a4dc611a9b7ebd2bb82a2f0207
stanislav.panov <Stanislav.Panov@leroymerlin.ru>
2025-04-11 09:29:46 +0300

    with ExecutorService

--- src/main/java/ru/spanov/spring/webflux/tutorial/controller/HelloWorldController.java
+++ src/main/java/ru/spanov/spring/webflux/tutorial/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package ru.spanov.spring.webflux.tutorial.controller;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tutorial")
 public class HelloWorldController {
 
+  private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+
   @GetMapping("/hello")
   public String sayHello() {
     return "Hello World!";
@@ -18,6 +23,32 @@ public class HelloWorldController {
 
   @GetMapping("/thread-info")
   public String threadInfo() {
+    try {
+      TimeUnit.MILLISECONDS.sleep(500);
+    } catch (InterruptedException e) {
+      log.error("Error:", e);
+    }
+    return String.format("""
+        thread: %s, \n
+        cpu: %s
+        """, Thread.currentThread(), Runtime.getRuntime().availableProcessors());
+  }
+
+  @GetMapping("/thread-info-with-executor")
+  public String threadInfoWithExecutor() {
+
+    var future1 = executorService.submit(this::getThreadInfo);
+    var future2 = executorService.submit(this::getThreadInfo);
+    String threadInfo = null;
+    try {
+      threadInfo = future1.get() + "\n" + future2.get();
+    } catch (InterruptedException | ExecutionException e) {
+      log.error("Error:", e);
+    }
+    return threadInfo;
+  }
+
+  private String getThreadInfo() {
     try {
       TimeUnit.MILLISECONDS.sleep(500);
     } catch (InterruptedException e) {

7f7cb72d73537ab86dc5f75b7d8d89dc8d4c78c2
stanislav.panov <Stanislav.Panov@leroymerlin.ru>
2025-06-24 17:14:35 +0300

    OFREP-806 конфигурация ConnectionProvider, HttpClient

--- spring-webflux-tutoarial-app/src/main/java/ru/spanov/spring/webflux/tutorial/app/controller/EligibilityController.java
+++ spring-webflux-tutoarial-app/src/main/java/ru/spanov/spring/webflux/tutorial/app/controller/EligibilityController.java
@@ -3,11 +3,14 @@ package ru.spanov.spring.webflux.tutorial.app.controller;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @RestController
 @RequestMapping("/tutorial/eligibility")
@@ -15,12 +18,30 @@ public class EligibilityController {
 
   private final WebClient ossWebClient = WebClient.builder()
       .baseUrl("https://oss-prep-marfak-a-stage.apps.lmru.tech")
+      .clientConnector(new ReactorClientHttpConnector(HttpClient.create(ConnectionProvider.builder("oss-pool")
+          .metrics(true)
+//          .maxConnectionPools(1)
+//          .maxConnections(700)
+//          .pendingAcquireMaxCount(100)
+          .build())))
       .build();
   private final WebClient ormWebClient = WebClient.builder()
       .baseUrl("https://orm-prep-marfak-a-stage.apps.lmru.tech")
+      .clientConnector(new ReactorClientHttpConnector(HttpClient.create(ConnectionProvider.builder("orm-pool")
+          .metrics(true)
+//          .maxConnectionPools(1)
+//          .maxConnections(700)
+//          .pendingAcquireMaxCount(100)
+          .build())))
       .build();
   private final WebClient lopusWebClient = WebClient.builder()
       .baseUrl("http://10.203.39.196:8090")
+      .clientConnector(new ReactorClientHttpConnector(HttpClient.create(ConnectionProvider.builder("lopus-pool")
+          .metrics(true)
+//          .maxConnectionPools(1)
+//          .maxConnections(700)
+//          .pendingAcquireMaxCount(100)
+          .build())))
       .build();
 
   @GetMapping("/byRegion")

9c6d53f07ce6162a93da7d9c445116c7a02c5621
stanislav.panov <Stanislav.Panov@leroymerlin.ru>
2025-06-24 08:25:21 +0300

    OFREP-806 call LOPUS stub

--- spring-webflux-tutoarial-app/src/main/java/ru/spanov/spring/webflux/tutorial/app/controller/EligibilityController.java
+++ spring-webflux-tutoarial-app/src/main/java/ru/spanov/spring/webflux/tutorial/app/controller/EligibilityController.java
@@ -15,11 +15,25 @@ public class EligibilityController {
   private final WebClient ormWebClient = WebClient.builder()
       .baseUrl("https://orm-prep-marfak-a-stage.apps.lmru.tech")
       .build();
+  private final WebClient lopusWebClient = WebClient.builder()
+      .baseUrl("http://10.203.39.196:8090")
+      .build();
 
   @GetMapping("/byRegion")
   public Mono<String> callSum() {
 //    return Mono.just(String.valueOf(1 + 2)).delayElement(ofMillis(500));
-    return ormWebClient
+
+   var lopusRequest = lopusWebClient
+        .post()
+        .uri("/productEligibility/byRegion")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(List.of(
+            new LopusRequestItem(22124025, 47),
+            new LopusRequestItem(11740111, 2)
+        ))
+        .retrieve()
+        .bodyToMono(String.class);
+    var ormRequest = ormWebClient
         .post()
         .uri("/best-offers")
         .contentType(MediaType.APPLICATION_JSON)
@@ -29,9 +43,15 @@ public class EligibilityController {
         ))
         .retrieve()
         .bodyToMono(String.class);
+
+    return Mono.zip(lopusRequest, ormRequest, (s1, s2) -> "Lopus: " + s1 + "\nORM: " + s2);
   }
 
   private record BestOfferRequestItem (Integer itemId, Integer zoneId) {
+
+  }
+
+  private record LopusRequestItem (Integer productId, Integer regionId) {
 
   }
 }
